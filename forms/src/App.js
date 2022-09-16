d31bda029679136d3a36ac410ec0c1cd551e78cc
Selina252000 <selina.darnal@netsmartz.net>
2022-09-16 17:33:23 +0530

    create form

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'antd/dist/antd.min.css';
+import {Form,Button,Checkbox,Input, Radio,Select,Row, Col} from "antd";
 
-function App() {
+
+export const App = () => {
+
+  const onClick = (values) =>{
+    console.log(values);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Row>
+      <Col span={10} offset = {7} > 
+        <Form
+        labelCol={{span: 8}}
+        wrapperCol={{span: 14}}
+        layout="horizontal"
+        
+      >
+      <Form.Item label="User Name"  name ="username">
+          <Input placeholder =  "enter your name"/>
+        </Form.Item>
+        
+        <Form.Item label="Gender">
+          <Radio.Group>
+            <Radio value="male"> Male </Radio>
+            <Radio value="female"> Female </Radio>
+          </Radio.Group>
+        </Form.Item>
+        
+        <Form.Item label="Select">
+          <Select>
+            <Select.Option value="demo1">audi</Select.Option>
+            <Select.Option value="demo2">bmw</Select.Option>
+          </Select>
+        </Form.Item>
+        
+        <Form.Item label="Chekbox" name="disabled" valuePropName="checked">
+          <Checkbox>Checkbox</Checkbox>
+        </Form.Item>
+        <Form.Item label="Button">
+          <Button   block type ="primary" onClick ={onClick}>Submit</Button>
+        </Form.Item>
+      </Form>
+      </Col>
+      </Row>
+
+    </>
+  )
 }
 
-export default App;
+export default  App;
